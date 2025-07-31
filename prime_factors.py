45f2017af0c2546d628467a1613ec98314bf2f66
gyuhong <jagijudo@gmail.com>
2025-07-31 12:33:04 +0900

    [feature] p.38

--- prime_factors.py
+++ prime_factors.py
@@ -3,5 +3,9 @@ class PrimeFactor:
     def of(cls, number) -> []:
         factors = []
         if number > 1:
-            factors.append(number)
+            if number == 4:
+                factors.append(2)
+                factors.append(2)
+            else:
+                factors.append(number)
         return factors
